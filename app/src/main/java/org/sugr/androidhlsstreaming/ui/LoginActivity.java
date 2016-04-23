8b8f927ca4ecd584a884addc9004e9594ae0593e
Viktor Kojouharov <developer@sugr.org>
2016-04-23 18:13:53 +0300

    initial login form

--- app/src/main/java/org/sugr/androidhlsstreaming/ui/LoginActivity.java
+++ app/src/main/java/org/sugr/androidhlsstreaming/ui/LoginActivity.java
@@ -1,4 +1,4 @@
-package org.sugr.androidhlsstreaming;
+package org.sugr.androidhlsstreaming.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.sugr.androidhlsstreaming.R;
 
 import java.util.ArrayList;
 import java.util.List;
