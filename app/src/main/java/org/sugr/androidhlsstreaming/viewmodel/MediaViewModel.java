0cc83ca614e566c44df35971a75d97f3938bb043
Viktor Kojouharov <developer@sugr.org>
2016-04-24 20:57:02 +0300

    show/hide the toolbar when clicking on the video

--- app/src/main/java/org/sugr/androidhlsstreaming/viewmodel/MediaViewModel.java
+++ app/src/main/java/org/sugr/androidhlsstreaming/viewmodel/MediaViewModel.java
@@ -1,9 +1,10 @@
 package org.sugr.androidhlsstreaming.viewmodel;
 
-import android.content.Context;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 import android.net.Uri;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.MediaController;
 
 import org.sugr.androidhlsstreaming.api.AuthService;
@@ -16,34 +17,64 @@ public class MediaViewModel implements ViewModel {
     public ObservableBoolean loading = new ObservableBoolean(true);
     public ObservableField<Uri> videoURI = new ObservableField<>();
 
-    private Context context;
+    private AppCompatActivity activity;
     private String email;
     private String auth;
     private AuthService service;
     private MediaActivityBinding binding;
     private MediaController mediaController;
+    private boolean toolbarVisible = true;
 
-    public MediaViewModel(Context context, String email, String auth, AuthService service, MediaActivityBinding binding) {
-        this.context = context;
+    public MediaViewModel(AppCompatActivity activity, String email, String auth, AuthService service, MediaActivityBinding binding) {
+        this.activity = activity;
         this.email = email;
         this.auth = auth;
         this.service = service;
         this.binding = binding;
-        this.mediaController = new MediaController(context);
+        this.mediaController = new MediaController(activity);
 
         mediaController.setMediaPlayer(binding.video);
         binding.video.setMediaController(mediaController);
         binding.video.requestFocus();
+        binding.video.setOnPreparedListener(mp -> {
+            loading.set(false);
+            hideToolbar();
+        });
+        binding.video.setOnTouchListener((v, event) -> {
+            if (toolbarVisible) {
+                hideToolbar();
+            } else {
+                showToolbar();
+            }
+            return false;
+        });
 
         fetchVideo();
     }
 
     @Override
     public void destroy() {
-        context = null;
+        activity = null;
         service = null;
         binding = null;
         mediaController = null;
+    }
+
+    private void showToolbar() {
+        ActionBar bar = activity.getSupportActionBar();
+        if (bar != null) {
+            bar.show();
+            toolbarVisible = true;
+        }
+
+    }
+
+    private void hideToolbar() {
+        ActionBar bar = activity.getSupportActionBar();
+        if (bar != null) {
+            bar.hide();
+            toolbarVisible = false;
+        }
     }
 
     private void fetchVideo() {
@@ -51,7 +82,6 @@ public class MediaViewModel implements ViewModel {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(uri -> {
-                    loading.set(false);
                     videoURI.set(uri);
                     binding.video.start();
                 }, err -> {
